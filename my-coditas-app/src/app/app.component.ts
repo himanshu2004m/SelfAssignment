cf1e77f7640eb0efc2b6cdc9a36a9b6b3f0b1ace
JETBLUE\HMaheshwari <himanshu.maheshwari@jetblue.com>
2018-06-22 20:21:41 +0530

    pagenation applied

--- my-coditas-app/src/app/app.component.ts
+++ my-coditas-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {GitUsers} from './user-model';
-import {SearchUserService} from './services/search-user.service';
+import { GitUsers } from './user-model';
+import { PagerServiceService } from './services/pager-service.service';
+import { SearchUserService } from './services/search-user.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
@@ -12,55 +13,61 @@ import 'rxjs/add/operator/map';
 export class AppComponent implements OnInit {
   title = 'app';
   data: any;
-  users: GitUsers[] = [
+  pagedItems: GitUsers[] = [
     {
-    'login': 'Himanshu',
-    'avatar_url': '../../assets/user3.jpg',
-    'url': 'Himanshu@april.biz',
-    'repos_url': '1',
-    'repo2' : '2'
+      'login': 'Himanshu',
+      'avatar_url': '../../assets/user3.jpg',
+      'url': 'Himanshu@april.biz',
+      'repos_url': '1',
+      'repo2': '2'
     },
     {
-    'login': 'Mohit',
-    'avatar_url': '../../assets/user1.jpg',
-    'url': 'Mohit@april.biz',
-    'repos_url': '1',
-    'repo2' : '2'
+      'login': 'Mohit',
+      'avatar_url': '../../assets/user1.jpg',
+      'url': 'Mohit@april.biz',
+      'repos_url': '1',
+      'repo2': '2'
     },
     {
 
-    'login': 'Ankur',
-    'avatar_url': '../../assets/user3.jpg',
-    'url': 'NA',
-    'repos_url': '1',
-    'repo2' : '2'
+      'login': 'Ankur',
+      'avatar_url': '../../assets/user3.jpg',
+      'url': 'NA',
+      'repos_url': '1',
+      'repo2': '2'
     }
 
   ];
-  singleUser: GitUsers = { login: '', url: '', repos_url: '', repo2: ''};
+  singleUser: GitUsers = { login: '', url: '', repos_url: '', repo2: '' };
 
+  flag = false;
+  // array of all items to be paged
+  private allItems: any[];
+  // pager object
+  pager: any = {};
 
-  constructor(private searchUser: SearchUserService) {
-
-  }
+  // paged items
+  pagedItems: any[];
+  constructor(private searchUser: SearchUserService, private pagerService: PagerServiceService) { }
 
   ngOnInit() {
-     this.searchUser.getUsers('himanshu2004m').subscribe((data: Response) => {
-         console.log('data', data);
-       }, function(error){
-         console.log(error);
-       });
+    this.searchUser.getUsers('himanshu2004m').subscribe((data: Response) => {
+      console.log('data', data);
+    }, function (error) {
+      console.log(error);
+    });
 
   }
 
-  onSearch(event:Event,text:String){
-console.log(text);
+  onSearch(event: Event, text: String) {
+    console.log(text);
     this.searchUser.getUsers(text).subscribe(users2 => {
       // set items to json response
 
-     this.data = users2;
-     this.users= this.data.items;
-      console.log(this.data.items);
+      this.data = users2;
+      this.users = this.data.items;
+      this.setPage(1);
+      console.log(this.data.items.length);
       // event.stopPropagation();
       // initialize to page 1
 
@@ -69,4 +76,17 @@ console.log(text);
 
   }
 
+  setPage(page: number) {
+    console.log(page);
+    if (page < 1 || page > this.pager.totalPages) {
+           return;
+          }
+
+    // get pager object from service
+    this.pager = this.pagerService.getPager(this.data.items.length, page);
+
+    // get current page of items
+    this.pagedItems = this.data.items.slice(this.pager.startIndex, this.pager.endIndex + 1);
+    console.log(this.pagedItems)
+  }
 }
--- my-coditas-app/src/app/app.module.ts
+++ my-coditas-app/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import {SearchUserService} from './services/search-user.service';
+import { PagerServiceService } from './services/pager-service.service';
+
 import { AppComponent } from './app.component';
 import { UserCardComponent } from './user-card/user-card.component';
 
@@ -14,7 +16,7 @@ import { UserCardComponent } from './user-card/user-card.component';
   imports: [
     BrowserModule, HttpClientModule
   ],
-  providers: [SearchUserService],
+  providers: [SearchUserService, PagerServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
